b28e200fd7986761683c48c688c9c8bfbe4a237e
Zhang JunLong <JunLong.Zhang@vatoptec.com>
2021-05-17 20:26:51 +0800

    学完第一章 Hello

---
+++ 1/GinHello/initRouter/initRouter.go
@@ -0,0 +1,15 @@
+package initRouter
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRouter() *gin.Engine {
+	router := gin.Default()
+	router.GET("/", func(context *gin.Context){
+		context.String(http.StatusOK, "hello gin")
+	})
+	return router
+}
